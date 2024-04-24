d52fbb86ca900fe0bc7f1aebdcbb44fc751a868c
EOJCH <milind.randive.ext@bayer.com>
2024-04-24 17:13:36 +0530

    custom development
    custom development files checked in.

---
+++ GraphAADAPI/GraphAADAPI/GraphAADAPI/GraphHelper.cs
@@ -0,0 +1,285 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using LanguageExt.Pipes;
+using LanguageExt.TypeClasses;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Microsoft.Graph.Models.ExternalConnectors;
+using System.Data.SqlClient;
+using System.Globalization;
+
+class GraphHelper
+{
+    // Settings object
+    private static Settings? _settings;
+    // App-ony auth token credential
+    private static ClientSecretCredential? _clientSecretCredential;
+    // Client configured with app-only authentication
+    private static GraphServiceClient? _appClient;
+
+    public static void InitializeGraphForAppOnlyAuth(Settings settings)
+    {
+        _settings = settings;
+
+        // Ensure settings isn't null
+        _ = settings ??
+            throw new System.NullReferenceException("Settings cannot be null");
+
+        _settings = settings;
+
+        if (_clientSecretCredential == null)
+        {
+            _clientSecretCredential = new ClientSecretCredential(
+                _settings.TenantId, _settings.ClientId, _settings.ClientSecret);
+        }
+
+        if (_appClient == null)
+        {
+            _appClient = new GraphServiceClient(_clientSecretCredential,
+                // Use the default scope, which will request the scopes
+                // configured on the app registration
+                new[] { "https://graph.microsoft.com/.default" });
+        }
+    }
+    public static async Task<string> GetAppOnlyTokenAsync()
+    {
+        // Ensure credential isn't null
+        _ = _clientSecretCredential ??
+            throw new System.NullReferenceException("Graph has not been initialized for app-only auth");
+
+        // Request token with given scopes
+        var context = new TokenRequestContext(new[] { "https://graph.microsoft.com/.default" });
+        //var context = new TokenRequestContext(new[] { "https://graph.microsoft.com/v1.0/users?" });
+
+        var response = await _clientSecretCredential.GetTokenAsync(context);
+        return response.Token;
+    }
+    public static Task<UserCollectionResponse?> GetUsersAsync()
+    {
+        // Ensure client isn't null
+        _ = _appClient ??
+            throw new System.NullReferenceException("Graph has not been initialized for app-only auth");
+
+
+        return _appClient.Users.GetAsync((config) =>
+        //return _appClient.Users[""].GetAsync((config)=> 
+        {
+            //config.QueryParameters.Filter = "startswith(OnPremisesSamAccountName,'MVVSE')";
+            //startsWith(userPrincipalName, 'username')") 
+
+            // config.QueryParameters.Count = true;
+            // config.QueryParameters.Filter = "identities / any(c: c / issuerAssignedId eq 'MVVSE' and c / issuer eq 'MVVSE')";
+            config.QueryParameters.Top = 999;
+           // config.QueryParameters.Filter = "imAddresses/any(i:i eq '_Chris_Turner@bayer.com')";
+
+            config.QueryParameters.Select = new[] { "displayName", "id", "onPremisesSamAccountName", "mail", "signInActivity", "lastPasswordChangeDateTime" };
+            // Get at most 25 results
+           // var c = config.QueryParameters.Count.Value.ToString();
+
+            // Sort by display name
+            // config.QueryParameters.Orderby = new[] { "displayName" };
+        });
+    }
+    public async static Task MakeGraphCallAsync()
+    {
+        // INSERT YOUR CODE HERE     
+       
+        try
+        {
+            SqlConnection SQLConnection = new SqlConnection();
+            SQLConnection.ConnectionString = "Data Source = BY-IAM-IDENTITY1.bayer.cnb,52560; Initial Catalog =IAM_HELPER; "
+                             + "Integrated Security=true;";
+
+            string query = String.Empty;
+            string query2 = "Truncate table tbl_180AAD";
+
+
+            SQLConnection.Open();
+            SqlCommand myCommand1 = new SqlCommand(query2, SQLConnection);
+            myCommand1.ExecuteNonQuery();
+            var messages = await _appClient.Users
+            .GetAsync((config) =>
+            {
+                config.QueryParameters.Top = 999;
+                config.QueryParameters.Filter = "accountEnabled eq true";
+                //config.QueryParameters.Filter = "identities/any(c:c/issuerAssignedId eq 'j.smith@yahoo.com' and c/issuer eq 'My B2C tenant')";
+                config.QueryParameters.Select = new[] { "displayName", "id", "onPremisesSamAccountName", "mail", "signInActivity", "lastPasswordChangeDateTime" };
+            });
+            string temp;
+            while (messages?.Value != null)
+            {
+                //foreach (var message in messages.Value)
+                //{
+                //    Console.WriteLine(message);
+                //    Console.WriteLine($"User: {message.DisplayName ?? "NO NAME"}");
+                //    Console.WriteLine($"  ID: {message.Id}");
+                //    Console.WriteLine($" sAMAccountName: {message.OnPremisesSamAccountName ?? "empty"}");
+
+
+                //    Console.WriteLine($"  Email: {message.Mail ?? "NO EMAIL"}");
+
+                //    if (message.SignInActivity != null)
+                //    {
+                //        Console.WriteLine($"  SignIn: {message.SignInActivity.LastSignInDateTime}");
+
+                //    }
+                //    if (message.LastPasswordChangeDateTime != null)
+                //    {
+                //        Console.WriteLine($" Password Last Set: {message.LastPasswordChangeDateTime.Value}");
+                //    }
+                //}
+
+                // If OdataNextLink has a value, there is another page
+                if (!string.IsNullOrEmpty(messages.OdataNextLink))
+                {
+                    // Pass the OdataNextLink to the WithUrl method
+                    // to request the next page
+                    messages = await _appClient.Users
+                        .WithUrl(messages.OdataNextLink)
+                        .GetAsync();
+                }
+                else
+                {
+                    // No more results, exit loop
+                    break;
+                }
+                foreach (var user in messages.Value)
+                {
+                    //user.SignInActivity.LastSignInDateTime;
+
+                    if (user.SignInActivity != null && user.LastPasswordChangeDateTime != null)
+                    {
+                        string dispNm = user.DisplayName.ToString();
+                        string usrid = user.Id.ToString();
+                        string OnpremSamAccount = null;
+                        if (user.OnPremisesSamAccountName!=null)
+                        {
+                            OnpremSamAccount = user.OnPremisesSamAccountName.ToString();
+                        }
+                        string email = null;
+                        if (user.Mail!= null)
+                        email= user.Mail.ToString();
+                        string AADLastlogin = user.SignInActivity.LastSignInDateTime.ToString();
+                        string AADPwdLastSet = user.LastPasswordChangeDateTime.ToString();
+                        string DummyDateValue = null;
+                        string DummyDateValue2 = null;
+                        if (AADLastlogin != null || AADPwdLastSet!=null)
+                        {
+                            DummyDateValue = AADLastlogin.Split()[0];
+                            DummyDateValue2 = AADPwdLastSet.Split()[0];
+                        }
+                        DateTime date =DateTime.Now;
+                        DateTime dt = DateTime.Now;
+                        if (DummyDateValue != "")
+                        {
+                             date= DateTime.ParseExact(DummyDateValue, "M/d/yyyy", CultureInfo.InvariantCulture);
+                            
+                        }
+
+                        if(DummyDateValue2!="")
+                            dt = DateTime.ParseExact(DummyDateValue2, "M/d/yyyy", CultureInfo.InvariantCulture);
+
+
+                        AADLastlogin = date.ToString("dd.MM.yyyy");
+                        AADPwdLastSet = dt.ToString("dd.MM.yyyy");
+                        try
+                        {
+                            query = "Insert into tbl_180AAD values (null, '" + usrid + "','" + OnpremSamAccount + "' , '" + email + "','" + AADLastlogin + "','" + AADPwdLastSet + "')";
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error getting users: {ex.Message}");
+                            Console.WriteLine(dispNm);
+                        }
+                        try
+                        {
+                            SqlCommand myCommand = new SqlCommand(query, SQLConnection);
+
+                            myCommand.ExecuteNonQuery();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error getting users: {ex.Message}");
+                            Console.WriteLine(dispNm);
+                        }
+                    }
+                    else
+                    {
+                        string dispNm = user.DisplayName.ToString();
+                        string usrid = user.Id.ToString();
+                        string OnpremSamAccount = null;
+                        if (user.OnPremisesSamAccountName != null)
+                        {
+                            OnpremSamAccount = user.OnPremisesSamAccountName.ToString();
+                        }
+
+
+                        //string email = user.Mail.ToString();
+                        string email = null;
+                        if (user.Mail != null)
+                            email = user.Mail.ToString();
+                        // string AADLastlogin = user.SignInActivity.LastSignInDateTime.ToString();
+                        // string AADPwdLastSet = user.LastPasswordChangeDateTime.ToString();
+                        // query = "Insert into tbl_180AAD values ('" + user.DisplayName + "', '" + user.Id + "' ,'" + user.OnPremisesSamAccountName + "','" + user.Mail + "',null,null)";
+                        try
+                        {
+
+                            query = "Insert into tbl_180AAD values (null, '" + usrid + "','" + OnpremSamAccount + "' , '" + email + "',null,null)";
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error getting users: {ex.Message}");
+                            Console.WriteLine(dispNm);
+                        }
+
+                        try
+                        {
+                            SqlCommand myCommand = new SqlCommand(query, SQLConnection);
+
+                            myCommand.ExecuteNonQuery();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error getting users: {ex.Message}");
+                            Console.WriteLine(dispNm);
+                        }
+                    }
+
+                    
+
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Error getting users: {ex.Message}");
+        }
+    }
+    public async static Task GetUsers()
+    {
+        
+           // var users =
+            //  .Request()
+            //.Filter("startswith(displayName,'Eric')")
+            //.Select("displayName,signInActivity")
+            //.GetAsync();
+            var result = await _appClient.Users.GetAsync((config) =>   //Users.GetAsync((requestConfiguration) =>
+            {
+                //config.QueryParameters.Select = new string[] { "displayName", "id" };
+                //config.QueryParameters.Filter = "identities/any(c:c/issuerAssignedId eq 'j.smith@yahoo.com' and c/issuer eq 'My B2C tenant')";
+                config.QueryParameters.Select = new[] { "displayName", "id", "onPremisesSamAccountName", "mail", "signInActivity", "lastPasswordChangeDateTime" };
+
+
+            });
+
+        var result1 = await _appClient.Users.GetAsync((config) =>
+        {
+           // requestConfiguration.QueryParameters.Filter = "startswith(displayName,'Eric')";
+            config.QueryParameters.Select = new string[] { "displayName", "signInActivity" };
+        });
+
+        //var users = await _appClient.Users.Request().GetAsync();
+
+
+    }
+}
---
+++ GraphAADAPI/GraphAADAPI/GraphAADAPI/Program.cs
@@ -0,0 +1,200 @@
+﻿using LanguageExt;
+using Microsoft.Graph.Models;
+using System.Data.SqlClient;
+using System.DirectoryServices;
+using System.Globalization;
+using System.Text;
+
+
+StringBuilder sb = new StringBuilder();
+
+Console.WriteLine(".NET Code to fetch User attributes \n");
+
+var settings = Settings.LoadSettings();
+
+// Initialize Graph
+InitializeGraph(settings);
+
+int choice = -1;
+
+//while (choice != 0)
+//{
+//    Console.WriteLine("Please choose one of the following options:");
+//    Console.WriteLine("0. Exit");
+//    Console.WriteLine("1. Display access token");
+//    Console.WriteLine("2. List users");
+//    Console.WriteLine("3. Make a Graph call");
+
+//    try
+//    {
+//        choice = int.Parse(Console.ReadLine() ?? string.Empty);
+//    }
+//    catch (System.FormatException)
+//    {
+//        // Set to invalid value
+//        choice = -1;
+//    }
+
+//    switch (choice)
+//    {
+//        case 0:
+//            // Exit the program
+//            Console.WriteLine("Goodbye...");
+//            break;
+//        case 1:
+//            // Display access token
+//            await DisplayAccessTokenAsync();
+//            break;
+//        case 2:
+//            // List users
+//            await ListUsersAsync();
+//            break;
+//        case 3:
+//            // Run any Graph code
+//            await MakeGraphCallAsync();
+//            break;
+//        default:
+//            Console.WriteLine("Invalid choice! Please try again.");
+//            break;
+//    }
+//}
+
+await MakeGraphCallAsync();
+
+void InitializeGraph(Settings settings)
+{
+    GraphHelper.InitializeGraphForAppOnlyAuth(settings);
+}
+
+async Task DisplayAccessTokenAsync()
+{
+    try
+    {
+        var appOnlyToken = await GraphHelper.GetAppOnlyTokenAsync();
+        Console.WriteLine($"App-only token: {appOnlyToken}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting app-only access token: {ex.Message}");
+    }
+}
+
+async Task ListUsersAsync()
+{
+    try
+    {
+        var userPage = await GraphHelper.GetUsersAsync();
+       //var users = await GraphHelper.Request().GetAsync();
+
+        if (userPage?.Value == null)
+        {
+            Console.WriteLine("No results returned.");
+            return;
+        }
+
+        // Output each users's details
+
+        while (userPage != null)
+        {
+            foreach (var user in userPage.Value)
+            {
+                Console.WriteLine($"User: {user.DisplayName ?? "NO NAME"}");
+                Console.WriteLine($"  ID: {user.Id}");
+                Console.WriteLine($" sAMAccountName: {user.OnPremisesSamAccountName ?? "empty"}");
+
+
+                Console.WriteLine($"  Email: {user.Mail ?? "NO EMAIL"}");
+
+                if (user.SignInActivity != null)
+                {
+                    Console.WriteLine($"  SignIn: {user.SignInActivity.LastSignInDateTime}");
+
+                }
+
+                Console.WriteLine($" Password Last Set: {user.LastPasswordChangeDateTime.Value}");
+                //Console.WriteLine($"Account status: {user.AccountEnabled.Value}");
+
+            }
+
+            if (!string.IsNullOrEmpty(userPage.OdataNextLink))
+            {
+                // Pass the OdataNextLink to the WithUrl method
+                // to request the next page
+               // userPage = await GraphHelper.GetUsersAsync()
+                 //   .WithUrl(messages.OdataNextLink)
+                 //   .GetAsync();
+            }
+            else
+            {
+                // No more results, exit loop
+                break;
+            }
+        }
+                
+
+        SqlConnection SQLConnection = new SqlConnection();
+        SQLConnection.ConnectionString = "Data Source = BY-IAM-IDENTITY1.bayer.cnb,52560; Initial Catalog =IAM_HELPER; "
+                         + "Integrated Security=true;";
+
+        string query = String.Empty;
+        string query2 = "Truncate table tbl_180AAD";
+       
+        
+        SQLConnection.Open();
+        SqlCommand myCommand1 = new SqlCommand(query2, SQLConnection);
+        myCommand1.ExecuteNonQuery();
+
+        foreach (var user in userPage.Value)
+        {
+            //user.SignInActivity.LastSignInDateTime;
+            
+            if (user.SignInActivity != null && user.LastPasswordChangeDateTime!=null)
+            {
+                string AADLastlogin = user.SignInActivity.LastSignInDateTime.ToString();
+                string AADPwdLastSet = user.LastPasswordChangeDateTime.ToString();
+                string DummyDateValue=null;
+                string DummyDateValue2=null;
+                //if (AADLastlogin != null)
+                //{
+                //     DummyDateValue = AADLastlogin.Split()[0];
+                //     DummyDateValue2 = AADLastlogin.Split()[0];
+                //}
+                                
+
+                //DateTime date = DateTime.ParseExact(DummyDateValue, "M/d/yyyy", CultureInfo.InvariantCulture);
+               // DateTime dt = DateTime.ParseExact(DummyDateValue2, "M/d/yyyy", CultureInfo.InvariantCulture);
+
+                //AADLastlogin = date.ToString("dd.MM.yyyy");
+                //AADPwdLastSet = dt.ToString("dd.MM.yyyy");
+
+                query = "Insert into tbl_180AAD values ('" + user.DisplayName + "', '" + user.Id + "','" + user.OnPremisesSamAccountName + "' , '" + user.Mail + "','" + AADLastlogin + "','" + AADPwdLastSet+"')";
+            }
+            else
+            {
+                query = "Insert into tbl_180AAD values ('" + user.DisplayName + "', '" + user.Id + "' ,'" + user.OnPremisesSamAccountName + "','" + user.Mail + "',null,null)";
+            }
+            
+            
+            SqlCommand myCommand = new SqlCommand(query, SQLConnection);
+           // myCommand1.ExecuteNonQuery();
+            myCommand.ExecuteNonQuery();
+            
+        }
+        SQLConnection.Close();
+
+
+
+        var moreAvailable = !string.IsNullOrEmpty(userPage.OdataNextLink);
+
+        Console.WriteLine($"\n More users available? {moreAvailable}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error getting users: {ex.Message}");
+    }
+}
+
+async Task MakeGraphCallAsync()
+{
+    await GraphHelper.MakeGraphCallAsync();
+}
